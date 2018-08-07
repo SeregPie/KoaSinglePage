495a5124d64732931f7f01f95feb670ceebf855a
Sergej Sintschilin <seregpie@gmail.com>
2018-08-07 16:55:40 +0200

    await next on error

--- KoaSinglePage.js
+++ KoaSinglePage.js
@@ -2,11 +2,15 @@ let KoaSend = require('koa-send');
 
 module.exports = function(root, {index = 'index.html'} = {}) {
 	let options = {root, index};
-	return async function(ctx) {
+	return async function(ctx, next) {
 		try {
 			await KoaSend(ctx, ctx.path, options);
 		} catch (error) {
-			await KoaSend(ctx, '/', options);
+			try {
+				await KoaSend(ctx, '/', options);
+			} catch (error) {
+				await next();
+			}
 		}
 	};
 };
