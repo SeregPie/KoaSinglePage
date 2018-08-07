c7c43208a4d962459c5a19cb3f7f85ee8333f925
Sergej Sintschilin <seregpie@gmail.com>
2018-08-07 16:52:54 +0200

    +

--- KoaSinglePage.js
+++ KoaSinglePage.js
@@ -1,12 +1,12 @@
-let send = require('koa-send');
+let KoaSend = require('koa-send');
 
 module.exports = function(root, {index = 'index.html'} = {}) {
 	let options = {root, index};
 	return async function(ctx) {
 		try {
-			await send(ctx, ctx.path, options);
+			await KoaSend(ctx, ctx.path, options);
 		} catch (error) {
-			await send(ctx, '/', options);
+			await KoaSend(ctx, '/', options);
 		}
 	};
 };
